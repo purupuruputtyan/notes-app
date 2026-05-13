c5df7605aa03138414a6c095394560a8862f3a46
MinakoMochizuki <m.mochizuki@growspirit.jp>
2026-05-13 11:20:17 +0900

    Not Foundのエラーメッセージも追加したいので命名を広めにする

--- backend/internal/domain/user/error.go
+++ backend/internal/domain/user/error.go
@@ -1,36 +1,36 @@
 package user
 
-type ValidateError struct {
+type AppError struct {
 	Code    string
 	Message string
 }
 
-func (e ValidateError) Error() string {
+func (e AppError) Error() string {
 	return e.Message
 }
 
 var (
-	ErrNickNameRequired = ValidateError{
+	ErrNickNameRequired = AppError{
 		Code:    "ERR_001",
 		Message: "ニックネームを入力してください",
 	}
 
-	ErrNickNameTooLong = ValidateError{
+	ErrNickNameTooLong = AppError{
 		Code:    "ERR_002",
 		Message: "ニックネームは20文字以内で入力してください",
 	}
 
-	ErrInvalidEmail = ValidateError{
+	ErrInvalidEmail = AppError{
 		Code:    "ERR_003",
 		Message: "メールアドレスの形式が正しくありません",
 	}
 
-	ErrPasswordTooShort = ValidateError{
+	ErrPasswordTooShort = AppError{
 		Code:    "ERR_004",
 		Message: "パスワードは8文字以上入力してください",
 	}
 
-	ErrInvalidPassword = ValidateError{
+	ErrInvalidPassword = AppError{
 		Code:    "ERR_005",
 		Message: "パスワードは英字・数字・記号を含めてください",
 	}
